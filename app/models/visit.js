3e5e39b08728409d5f5f2bd9be5bd6db591e2922
James <james.pereira90@outlook.com>
2017-06-19 21:39:49 +1000

    update visit with validation

--- app/models/visit.js
+++ app/models/visit.js
@@ -4,5 +4,13 @@ export default DS.Model.extend({
   company: DS.attr('string'),
   name: DS.attr('string'),
   email: DS.attr('string'),
-  who: DS.attr('string')
+  who: DS.attr('string'),
+
+  isValidEmail: Ember.computed.match('email', /^.+@.+\..+$/),
+  isNameLongEnough: Ember.computed.gte('name.length', 1),
+  isCompanyLongEnough: Ember.computed.gte('company.length', 1),
+  isWhoLongEnough: Ember.computed.gte('who.length', 1),
+
+  isValid: Ember.computed.and('isValidEmail', 'isNameLongEnough', 'isCompanyLongEnough', 'isWhoLongEnough'),
+  isNotValid: Ember.computed.not('isValid')
 });
